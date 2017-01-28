c576b5d60d2a2e2f7d8e6dfb4541137cbd22ba34
Dmitry <naym333@gmail.com>
2017-01-28 20:52:40 +0500

    много всего сделано, и в порыве написания кода закомичено в один комит

--- asset/app.js
+++ asset/app.js
@@ -3,12 +3,13 @@
         el: "#gitlabTimer",
         data: {
             config: {
-                gitlabHost: null,
-                privateKey: null,
+                gitlab: {
+                    host: null,
+                    privateKey: null,
+                }
             },
             isGitlabSupportedTimetracking: false,
             showTimeTrackingNotice: false,
-            isAuthorized: false,
             showAuthDialog: false,
             projectList: [],
             issueList: [],
@@ -21,57 +22,20 @@
             timerActiveString: null,
             timerUpdateInterval: null,
             showTimerPreloader: false,
+            gitlab: null,
+            errorText: null
+
         },
         methods: {
             getUrl: function (path) {
-                return this.config.gitlabHost + path;
-            },
-            checkGitlabVersion: function () {
-                this.showPreloader = true;
-                $.get(this.config.gitlabHost + '/api/v3/version', function (response) {
-                    gitlabTimer.showPreloader = false;
-                    gitlabTimer.isGitlabSupportedTimetracking = response.version.split('-')[0] >= '8.16.1';
-                    gitlabTimer.showTimeTrackingNotice = !gitlabTimer.isGitlabSupportedTimetracking
-                    if (!gitlabTimer.isGitlabSupportedTimetracking) {
-                        gitlabTimer.$destroy();
-                    }
-
-                    gitlabTimer.loadProjectList()
-                }, 'json');
-            },
-            checkPrivateKey: function () {
-                if (!this.config.privateKey) {
-                    return false;
-                }
-                this.setPrivateKey();
-                this.showPreloader = true;
-                    $.get(this.config.gitlabHost + '/api/v3/version', function (response) {
-                        if (response.version) {
-                            gitlabTimer.rememberPrivateKey();
-                            gitlabTimer.isAuthorized = true;
-                            gitlabTimer.showAuthDialog = false;
-                        }
-                        gitlabTimer.showPreloader = false;
-                        gitlabTimer.checkGitlabVersion();
-                    }).fail (function() {
-                        gitlabTimer.showPreloader = false;
-                        gitlabTimer.showAuthDialog = true;
-                    });
-
-            },
-            setPrivateKey: function () {
-                $.ajaxSetup({
-                    headers: {'PRIVATE-TOKEN': this.config.privateKey}
-                });
-            },
-            rememberPrivateKey: function () {
-                localStorage.setItem('privateKey', this.config.privateKey);
+                return this.config.gitlab.host + path;
             },
             loadProjectList: function () {
                 this.showPreloader = true;
-                $.getJSON(gitlabTimer.getUrl('/api/v3/projects', {per_page: 100}), function (response) {
-                    gitlabTimer.projectList = response;
+                this.gitlab.getProjects().then(function (response) {
+                    gitlabTimer.projectList = response.body;
                     gitlabTimer.showPreloader = false;
+                    gitlabTimer.showIssueList = false;
                     gitlabTimer.showProjectList = true;
                 })
             },
@@ -81,23 +45,29 @@
             },
             setProject: function (id) {
                 this.currentProject = id;
+                this.showProjectList = false;
                 this.loadIssueList();
-                gitlabTimer.showProjectList = false;
             },
             loadIssueList: function () {
-                this.showPreloader = true;
-                var path = '/api/v3/projects/' + this.currentProject + '/issues'
-                $.getJSON(gitlabTimer.getUrl(path), {state: "opened", per_page: 100}, function (response) {
-                    gitlabTimer.issueList = response;
-                    gitlabTimer.showPreloader = false;
-                    gitlabTimer.showIssueList = true
-                })
+                var self = this;
+                self.showPreloader = true;
+
+                this.gitlab.getOpenedIssues(this.currentProject).then(function (response) {
+                    var issues = response.body;
+
+                    self.issueList = issues;
+                    self.showPreloader = false;
+                    self.showIssueList = true
+                });
             },
             startTimer: function (issueId) {
+                if (this.currentIssue){
+                    this.stopTimer();
+                }
                 this.currentIssue = issueId;
                 this.timer = new timer();
 
-                $(window).on('beforeunload', function(e){
+                $(window).on('beforeunload', function (e) {
                     return null;
                 }, false);
 
@@ -128,44 +98,113 @@
                 }, 1000)
             },
             stopTimer: function () {
+                var self = this;
+                var issue = self.currentIssue;
+                self.currentIssue = null;
 
-                var issue = this.currentIssue;
-                this.currentIssue = null;
-                this.timer.stop();
-                clearInterval(this.timerUpdateInterval);
-                this.timerUpdateInterval = null;
+                self.timer.stop();
+
+                clearInterval(self.timerUpdateInterval);
+                self.timerUpdateInterval = null;
+
                 $(window).off('beforeunload');
 
-                var spentTime = Math.floor(this.timer.getTimeInSeconds() / 60);
-                if (spentTime){
-                    this.showTimerPreloader = true;
-                    var path = "/api/v3/projects/"+this.currentProject+"/issues/"+issue+"/add_spent_time?duration="+spentTime.toString()+"m";
-                    $.post(this.getUrl(path), function(){
-                        gitlabTimer.showTimerPreloader = false;
+                var spentTime = Math.floor(self.timer.getTimeInSeconds() / 60);
+                if (spentTime) {
+                    self.showTimerPreloader = true;
+                    self.gitlab.spentTime(self.currentProject, issue, spentTime.toString() + "m").then(function () {
+                        self.showTimerPreloader = false;
                     });
                 }
             },
             isActiveIssue: function (issueId) {
                 return this.currentIssue == issueId;
+            },
+            applyConfig: function () {
+                if (!this.config.gitlab.host || !this.config.gitlab.privateKey) {
+                    this.errorText = 'Неверные настройки';
+                    return;
+                }
+                // Сохраняем конфиг
+                var serializedConfig = JSON.stringify(gitlabTimer.config);
+                localStorage.setItem('config', serializedConfig);
+
+                this.initApplication();
+            },
+            initApplication: function(){
+                var self = this;
+                // load configs
+                var configData = localStorage.getItem('config');
+                var config = JSON.parse(configData);
+                var configIsValid = !!config && !!config.gitlab.host && !!config.gitlab.privateKey;
+
+                if (configIsValid) {
+                    this.config = config;
+                    this.gitlab = new gitlabApi(this.config.gitlab.host, this.config.gitlab.privateKey);
+                    this.testConfig().then(function (data) {
+                        self.showPreloader = false;
+
+                        if (!data.privateKeyValid) {
+                            self.errorText = "Gitlab PrivateKey не активен. Измените его в настройках";
+                            return;
+                        }
+                        if (!data.gitlabVersionSupported) {
+                            self.errorText = "К сожалению Ваша версия gitlab не потдерживает time tracking";
+                            return;
+                        }
+                        self.errorText = null;
+
+                        self.loadProjectList();
+                    }, function (data) {
+                        if (data.status === 401) {
+                            self.errorText = "Неверный Gitlab PrivateKey. Введите новый в настройках";
+                        } else {
+                            self.errorText = "Произошла ошибка при попытке подключения к серверу " + self.config.gitlab.host;
+                        }
+                    });
+                } else {
+                    var $modal =$(self.$el).find('#settingsDialog');
+                    if (!$modal.is('.show')) {
+                        $modal.modal('show');
+                    }
+                }
+            },
+            testConfig: function () {
+                var self = this;
+                var promise = new Promise(function (resolve, reject) {
+                    var result = {
+                        privateKeyValid: undefined,
+                        gitlabVersionSupported: undefined,
+                    };
+                    self.gitlab.getVersion().then(function (response) {
+                        // Проверяем авторизацию
+                        var body = response.body;
+                        var version = body.version;
+                        if (version) {
+                            result.privateKeyValid = true;
+                        } else {
+                            result.privateKeyValid = false;
+                        }
+
+                        // проверяем версию gitlab
+                        result.gitlabVersionSupported = version.split('-')[0] >= '8.16.1';
+                        resolve(result);
+                    }, function (d) {
+                        reject(d);
+                    });
+                });
+
+                return promise;
+
             }
         },
         computed: {
             getPrivateKeyUrl: function () {
-                return this.config.gitlabHost + '/profile/account';
+                return this.getUrl('/profile/account');
             }
         },
         mounted: function () {
-            // load config file
-            $.getJSON('./config.json', function (response) {
-                gitlabTimer.config.gitlabHost = response.gitlabHost;
-                var privateKey = localStorage.getItem('privateKey');
-                if (privateKey) {
-                    gitlabTimer.config.privateKey = privateKey;
-                    gitlabTimer.checkPrivateKey();
-                } else {
-                    gitlabTimer.showAuthDialog = true;
-                }
-            });
+            this.initApplication()
         },
 
     });
@@ -191,4 +230,60 @@
             getTimeInSeconds: getTimeInSeconds
         }
     }
+
+    function gitlabApi(server, privatekey) {
+
+        if (!server) {
+            throw new Error('Не указан сервер gitlab')
+        }
+        if (!privatekey) {
+            throw new Error('Не указан приватный ключ')
+        }
+
+        var requestOptions = {
+            headers: {'PRIVATE-TOKEN': privatekey}
+        };
+
+        var get = function (path, body) {
+            var url = server + path;
+            var opts = JSON.parse(JSON.stringify(requestOptions));
+            opts.params = body;
+
+            return Vue.http.get(url, opts);
+        }
+
+
+        var post = function (path, data) {
+            var url = server + path;
+            return Vue.http.post(url, data, requestOptions);
+        }
+
+        var getVersion = function () {
+            return get('/api/v3/version');
+        };
+
+        var getProjects = function () {
+            return get('/api/v3/projects?per_page=100');
+        }
+
+        var getOpenedIssues = function (projectId) {
+            var path = '/api/v3/projects/' + projectId + '/issues'
+            return get(path, {state: "opened", per_page: 100});
+        }
+
+        var spentTime = function (projectId, issueId, time) {
+            var path = "/api/v3/projects/" + projectId + "/issues/" + issueId + "/add_spent_time?duration=" + time
+            return post(path);
+        }
+
+
+        return {
+            getVersion: getVersion,
+            getProjects: getProjects,
+            getOpenedIssues: getOpenedIssues,
+            spentTime: spentTime
+        }
+    }
+
+    window.gitlabApi = gitlabApi;
 }());
